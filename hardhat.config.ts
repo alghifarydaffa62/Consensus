49e54bc3ab5384fc478547daef1ddeac5f7c5983
M Daffa A <daffa.alghifary62@gmail.com>
2025-12-11 21:21:20 +0700

    feat: setup hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,30 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ignition";
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable, defineConfig } from "hardhat/config";
+import * as dotenv from "dotenv"
 
-export default defineConfig({
+dotenv.config()
+
+const config: HardhatUserConfig = {
   plugins: [hardhatToolboxMochaEthersPlugin],
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
+      viaIR: true,
     },
   },
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: process.env.ALCHEMY_RPC_URL || "",
+      accounts: [process.env.WALLET_PRIVATE_KEY!],
     },
   },
-});
+};
+
+export default config;
