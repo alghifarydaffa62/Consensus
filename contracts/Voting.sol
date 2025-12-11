a0ad06fd55bf1875b9a6b0222454021bacc6581a
M Daffa A <daffa.alghifary62@gmail.com>
2025-12-11 21:12:45 +0700

    feat: smart contract Voting and Factory

---
+++ contracts/Voting.sol
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+  string public metadataURI;
+  uint public votingEndTime;
+  bool public isVotingStarted;
+
+  struct Voter {
+    uint votedCandidate;
+    bool hasVoted;   
+    bool isRegistered;
+  }
+
+  struct Candidate {
+    uint candidateID;
+    uint totalVote;
+    bool isRegistered;
+  }
+
+  mapping(address => Voter) public voters;
+  mapping(address => Candidate) public candidates;
+  address[] public candidateAddresses;
+
+  event registerVoterSuccess(address indexed admin, address indexed voter);
+  event registerCandidateSuccess(address indexed admin, address indexed candidate);
+  event castVoteSuccess(address indexed voter, uint indexed candidateVoted);
+  event VotingStarted(address indexed admin);
+
+  constructor(string memory _metadataURI) Ownable(msg.sender) {
+      metadataURI = _metadataURI;
+      isVotingStarted = false;
+      votingEndTime = 0;
+  }
+
+  modifier onlyPending() {
+      require(!isVotingStarted, "Voting has already started!");
+      _;
+  }
+
+  modifier onlyActive() {
+      require(isVotingStarted, "Voting has not started yet!");
+      require(block.timestamp < votingEndTime, "Voting has ended!");
+      _;
+  }
+
+  modifier onlyEnded() {
+      require(isVotingStarted, "Voting has not started yet!");
+      require(block.timestamp >= votingEndTime, "voting is still ongoing!");
+      _;
+  }
+
+  function registerInternal(address _voter) private returns(bool) {
+      if(_voter == address(0) || voters[_voter].isRegistered) {
+          return false;
+      }
+
+      voters[_voter] = Voter({
+          votedCandidate: 0,
+          hasVoted: false,
+          isRegistered: true
+      });
+      emit registerVoterSuccess(msg.sender, _voter);
+      return true;
+  }
+
+  function registerVoter(address _voter) external onlyOwner onlyPending {
+      bool success = registerInternal(_voter);
+      require(success, "Invalid address or already registered!");
+  }
+
+  function registerVoterBatch(address[] calldata _voters) external onlyOwner onlyPending {
+      for(uint i = 0; i < _voters.length; i++) {
+          registerInternal(_voters[i]);
+      }
+  }
+
+  function registerCandidate(address _candidateAdd) external onlyOwner onlyPending {
+      require(!candidates[_candidateAdd].isRegistered, "Candidate already registered!");
+      require(!voters[_candidateAdd].isRegistered, "Voter cannot be a candidate!");
+      require(_candidateAdd != address(0), "Address invalid!");
+
+      uint newID = candidateAddresses.length;
+      candidates[_candidateAdd] = Candidate({
+          candidateID: newID,
+          totalVote: 0,
+          isRegistered: true
+      });
+
+      candidateAddresses.push(_candidateAdd);
+
+      emit registerCandidateSuccess(msg.sender, _candidateAdd);
+  }
+
+  function startVoting(uint _votingEndTime) external onlyOwner onlyPending{
+      require(_votingEndTime > block.timestamp, "Invalid duration!");
+
+      isVotingStarted = true;
+      votingEndTime = _votingEndTime;
+
+      emit VotingStarted(msg.sender);
+  }
+
+  function castVote(uint candidateId) external onlyActive {
+      require(voters[msg.sender].isRegistered, "You are not registered!");
+      require(!voters[msg.sender].hasVoted, "You already vote!");
+      require(candidateId < candidateAddresses.length, "Invalid ID!");
+
+      address votedAddr = candidateAddresses[candidateId];
+      candidates[votedAddr].totalVote++;
+      voters[msg.sender].votedCandidate = candidateId;
+      voters[msg.sender].hasVoted = true;
+
+      emit castVoteSuccess(msg.sender, candidateId);
+  }
+
+  function getVotingStatus() external view returns(string memory) {
+      if(!isVotingStarted) {
+          return "Pending";
+      }
+
+      if(block.timestamp >= votingEndTime) {
+          return "Closed";
+      } else {
+          return "Active";
+      }
+  }
+
+  function getRemainingTime() external view returns (uint256) {
+      if(!isVotingStarted || block.timestamp >= votingEndTime) {
+          return 0;
+      }
+
+      return votingEndTime - block.timestamp;
+  }
+
+  function showProgress(uint _offset, uint _limit) external view onlyActive returns(uint[] memory, uint[] memory) {
+      require(_limit > 0, "Limit must be greater than zero!");
+
+      uint totalCandidates = candidateAddresses.length;
+      uint endIndex = _offset + _limit;
+
+      if(endIndex > totalCandidates) {
+          endIndex = totalCandidates;
+      }
+
+      uint actualSize = endIndex - _offset;
+      uint[] memory candidateIds = new uint[](actualSize);
+      uint[] memory total = new uint[](actualSize);
+
+      uint j = 0;
+      for(uint i = _offset; i < endIndex; i++) {
+          address candidateAddr = candidateAddresses[i];
+          candidateIds[j] = candidates[candidateAddr].candidateID;
+          total[j] = candidates[candidateAddr].totalVote;
+
+          j++;
+      } 
+
+      return (candidateIds, total);
+  }
+
+  function showResult(uint _offset, uint _limit) external view onlyEnded returns (uint[] memory, uint[] memory) {
+      require(_limit > 0, "Limit must be greater than zero!");
+      
+      uint totalCandidates = candidateAddresses.length;
+      uint endIndex = _offset + _limit;
+
+      if(endIndex > totalCandidates) {
+          endIndex = totalCandidates;
+      }
+
+      uint actualSize = endIndex - _offset;
+      uint[] memory candidateIds = new uint[](actualSize);
+      uint[] memory votes = new uint[](actualSize);
+
+      uint j = 0;
+      
+      for(uint i = _offset; i < endIndex; i++) {
+          address candidateAddr = candidateAddresses[i];
+          candidateIds[j] = candidates[candidateAddr].candidateID;
+          votes[j] = candidates[candidateAddr].totalVote;
+
+          j++;
+      }
+
+      return (candidateIds, votes);
+  }
+}
---
+++ test/VotingTest.ts
@@ -0,0 +1,325 @@
+import { expect } from "chai";
+import { network } from "hardhat";
+import type { Contract, Signer } from "ethers";
+const { ethers } = await network.connect();
+
+describe("Voting Testing", function () {
+  let Voting: any;
+  let voting: Contract;
+  let owner: Signer;
+  let addr1: Signer;
+  let addr2: Signer;
+
+  let now: number;
+  let futureTime: number;
+
+  async function getLatestTime(): Promise<number> {
+    const blockNum = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(blockNum);
+    if (!block) throw new Error("Block not found");
+    return block.timestamp;
+  }
+
+  async function increaseTime(seconds: number) {
+    await ethers.provider.send("evm_increaseTime", [seconds]);
+    await ethers.provider.send("evm_mine", []);
+  }
+
+  beforeEach(async () => {
+    Voting = await ethers.getContractFactory("Voting");
+    [owner, addr1, addr2] = await ethers.getSigners();
+    voting = await Voting.deploy("ipfs://QmDummyHash123");
+  })
+
+  async function startVotingHelper() {
+    now = await getLatestTime()
+    futureTime = now + 3600
+
+    await voting.startVoting(futureTime)
+  }
+
+  describe("deployment", () => {
+    it("Happy Path: Should set the right metadataURI", async () => {
+      expect(await voting.metadataURI()).to.equal("ipfs://QmDummyHash123")
+    })
+
+    it("Happy Path: Should have votingEndTime as 0", async () => {
+      expect(await voting.votingEndTime()).to.equal(0)
+    })
+  })
+
+  describe("registerVoter", () => {
+    it("Happy Path: should register a voter successfully", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+      const voterData = await voting.voters(await addr1.getAddress());
+      expect(voterData.isRegistered).to.be.true;
+    });
+
+    it("Sad Path: Should REVERT if trying to register the same voter twice", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+
+      await expect(
+        voting.registerVoter(await addr1.getAddress())
+      ).to.be.revertedWith("Invalid address or already registered!");
+    });
+
+    it("Sad Path: Should REVERT if non-owner tries to register a voter", async () => {
+      await expect(
+        voting.connect(addr1).registerVoter(await addr2.getAddress())
+      ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount");
+    });
+    
+    it("Sad Path: Should REVERT if trying to register the zero address", async () => {
+      await expect(
+        voting.registerVoter(ethers.ZeroAddress)
+      ).to.be.revertedWith("Invalid address or already registered!");
+    })
+
+    it("Sad Path: Should REVERT if owner tries to register after voting has started", async () => {
+      await startVotingHelper();
+
+      await expect(
+        voting.registerVoter(await addr1.getAddress())
+      ).to.be.revertedWith("Voting has already started!");
+    })
+  });
+
+  describe("RegisterCandidate", () => {
+    it("Happy Path: should register a candidate successfully", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+
+      const candidateData = await voting.candidates(await addr1.getAddress());
+      expect(candidateData.isRegistered).to.be.true;
+    })
+
+    it("Sad Path: Should REVERT if trying to register the same candidate twice", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+
+      await expect(
+        voting.registerCandidate(await addr1.getAddress())
+      ).to.be.revertedWith("Candidate already registered!");
+    })
+
+    it("Sad Path: Should REVERT if non-owner tries to register a candidate", async () => {
+      await expect(
+        voting.connect(addr1).registerCandidate(await addr2.getAddress())
+      ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount");
+    })
+
+    it("Sad Path: Should REVERT if trying to register a voter as a candidate", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+
+      await expect(
+        voting.registerCandidate(await addr1.getAddress())
+      ).to.be.revertedWith("Voter cannot be a candidate!");
+    })
+
+    it("Sad Path: Should REVERT if trying to register the zero address", async () => {
+      await expect(
+        voting.registerCandidate(ethers.ZeroAddress)
+      ).to.be.revertedWith("Address invalid!");
+    })
+
+    it("Sad Path: Should REVERT if owner tries to register candidate after voting has started", async () => {
+      await startVotingHelper();
+
+      await expect(
+        voting.registerCandidate(await addr1.getAddress())
+      ).to.be.revertedWith("Voting has already started!");
+    })
+  })
+  
+  describe("Cast Voting", () => {
+    
+    it("Happy Path: Should cast a vote successfully", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+      
+      await voting.connect(addr1).castVote(0);
+
+      const candidateData = await voting.candidates(await addr2.getAddress());
+      expect(candidateData.totalVote).to.equal(1);
+
+      const voterData = await voting.voters(await addr1.getAddress());
+      expect(voterData.hasVoted).to.be.true;
+      expect(voterData.votedCandidate).to.equal(0);
+    })
+
+    it("Sad Path: Should REVERT if unregistered voter tries to cast a vote", async () => {
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+
+      await expect(
+        voting.connect(addr1).castVote(0)
+      ).to.be.revertedWith("You are not registered!");
+    })
+
+    it("Sad Path: Should REVERT if voter tries to vote when voting has not started", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+
+      await expect(
+        voting.connect(addr1).castVote(0)
+      ).to.be.revertedWith("Voting has not started yet!");
+    })
+
+    it("Sad Path: Should REVERT if voter tries to vote when voting has ended", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+
+      await increaseTime(4000)
+
+      await expect(
+        voting.connect(addr1).castVote(0)
+      ).to.be.revertedWith("Voting has ended!");
+    })
+
+    it("Sad Path: Should REVERT if voter tries to vote twice", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+
+      await voting.connect(addr1).castVote(0);
+
+      await expect(
+        voting.connect(addr1).castVote(0)
+      ).to.be.revertedWith("You already vote!");
+    })
+
+    it("Sad Path: Should REVERT if voter tries to vote for unregistered candidate", async () => {
+      await voting.registerVoter(await addr1.getAddress());
+      await startVotingHelper();
+
+      await expect(
+        voting.connect(addr1).castVote(99)
+      ).to.be.revertedWith("Invalid ID!");
+    })
+  })
+
+  describe("start voting", () => {
+    it("Happy Path: shoud start voting successfully", async () => {
+      const now = await getLatestTime()
+      const endTime = now + 3600;
+
+      await voting.startVoting(endTime);
+      
+      expect(await voting.isVotingStarted()).to.be.true;
+      expect(await voting.votingEndTime()).to.equal(endTime);
+    })
+
+    it("Sad Path: Should REVERT if non-owner tries to start voting", async () => {
+      const now = await getLatestTime()
+      await expect(
+        voting.connect(addr1).startVoting(now + 3600)
+      ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount");
+    })
+
+    it("Sad Path: Should REVERT owner trying to start voting twice", async () => {
+      await startVotingHelper();
+      const now = await getLatestTime()
+      const endTime = now + 3600;
+
+      await expect(
+        voting.startVoting(endTime)
+      ).to.be.revertedWith("Voting has already started!");
+    })
+  })
+
+  describe("Show Result", () => {
+    it("Happy Path: Pagination should work", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+
+      await increaseTime(4000);
+
+      const [candidateIds, votes] = await voting.showResult(0, 1);
+      expect(candidateIds.length).to.equal(1);
+      expect(candidateIds[0]).to.equal(0);
+
+      const [candidateIds2, votes2] = await voting.showResult(1, 1);
+      expect(candidateIds2.length).to.equal(1);
+      expect(candidateIds2[0]).to.equal(1);
+    })
+
+    it("Sad Path: Should REVERT if show result called before voting is start", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+
+      await expect(
+        voting.showResult(0, 1)
+      ).to.be.revertedWith("Voting has not started yet!");
+    })
+
+    it("Sad Path: Should REVERT if show result called before voting is closed", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+
+      await expect(
+        voting.showResult(0, 1)
+      ).to.be.revertedWith("voting is still ongoing!");
+    })
+
+    it("Sad Path: Should REVERT if limit pagination are zero", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+      await increaseTime(4000)
+
+      await expect(
+        voting.showResult(0, 0)
+      ).to.be.revertedWith("Limit must be greater than zero!");
+    })
+  })
+
+  describe("Show Progress", () => {
+    it("Happy Path: Should show progress successfully", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+
+      const [candidateIDs, votes] = await voting.showProgress(0, 1);
+      expect(candidateIDs.length).to.equal(1);
+      expect(candidateIDs[0]).to.equal(0);
+      expect(votes[0]).to.equal(0);
+
+      const [candidateIDs2, votes2] = await voting.showProgress(1, 1);
+      expect(candidateIDs2.length).to.equal(1);
+      expect(candidateIDs2[0]).to.equal(1);
+      expect(votes2[0]).to.equal(0);
+    })
+
+    it("Sad Path: Should REVERT if show progress called before voting is start", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+
+      await expect(
+        voting.showProgress(0, 1)
+      ).to.be.revertedWith("Voting has not started yet!");
+    })
+
+    it("Sad Path: Should REVERT if show progress called when voting is closed", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+      await increaseTime(4000)
+
+      await expect(
+        voting.showProgress(0, 1)
+      ).to.be.revertedWith("Voting has ended!");
+    })
+
+    it("Sad Path: Should REVERT if limit pagination are zero", async () => {
+      await voting.registerCandidate(await addr1.getAddress());
+      await voting.registerCandidate(await addr2.getAddress());
+      await startVotingHelper();
+      
+      await expect(
+        voting.showProgress(0, 0)
+      ).to.be.revertedWith("Limit must be greater than zero!");
+    })
+  })
+});
